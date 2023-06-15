4acabfd1db2a250bd1bb8290d66211dc7f53118e
Big Andy <60822006+andrewlkd@users.noreply.github.com>
2023-06-15 20:26:01 +0100

    Correct file loading in NAE

--- formula/nae/naef.py
+++ formula/nae/naef.py
@@ -1,7 +1,9 @@
 from typing import List
+from pysat.formula import CNF as PySATCNF
 
 from formula.cnf.cnf import CNF
 from formula.nae.nae_clause import NAEClause
+from formula.variable import Variable
 
 
 class NAEFormula(CNF):
@@ -22,3 +24,33 @@ class NAEFormula(CNF):
         """
         assert isinstance(clause, NAEClause)
         super().append(clause)
+
+    @classmethod
+    def from_pysat(cls, formula: PySATCNF) -> None:
+        """Creates formula from pysat formula.
+
+        Args:
+            formula (PySATFormula): Pysat formula to use.
+        """
+
+        clauses = []
+        for clause in formula.clauses:
+            c = NAEClause()
+            for var in clause:
+                # PySat starts numbering at 1 and uses negative to indicate negation
+                v = Variable(abs(var) - 1, is_negation=var < 0)
+                c.append(v)
+            clauses.append(c)
+        return cls(clauses)
+
+    @classmethod
+    def from_file(cls, filename: str) -> None:
+        """Creates formula from CNF file.
+
+        Args:
+            filename (str): File to read from.
+        """
+
+        # Use PySAT as interface
+        pcnf = PySATCNF(from_file=filename)
+        return NAEFormula.from_pysat(pcnf)
